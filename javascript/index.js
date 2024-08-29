a7fda640cadd6daa4fd2296468e8fee5201146cd
Valerio Persiani <persianivalerio0@gmail.com>
2024-08-29 17:55:17 +0200

    completamento di javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,22 @@
-function addTask() {
-  const inputTask = document.getElementById("input");
-  const inputText = inputTask.value;
-
-  const divTask = document.createElement("div");
-  divTask.textContent = inputText;
-  const lastTask = document.getElementById("lastTask");
-  lastTask.appendChild(divTask);
-}
-addTask();
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("addButton");
+  button.addEventListener("click", function () {
+    const input = document.getElementById("input");
+    const text = input.value.trim();
+    if (text) {
+      const li = document.createElement("li");
+      li.textContent = text;
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "ELIMINA";
+      deleteButton.addEventListener("click", function () {
+        li.remove();
+      });
+      li.appendChild(deleteButton);
+      document.getElementById("taskList").appendChild(li);
+      input.value = "";
+    } else {
+      alert("Non hai inserito il testo");
+    }
+    console.log("clic avvenuto");
+  });
+});
